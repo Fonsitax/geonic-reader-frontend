3a526b310a81e9e1f1f541ff321909f3d86857cd
Fonsitax <mynameisfonja@gmail.com>
2024-08-22 16:31:42 +0200

    css stuff and search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { getAllReadings, getReadingByCountry, getReadingById } from '../services/readings-service';
+import SearchBar from '../components/Search';
 
 const Home = () => {
 
@@ -22,6 +23,12 @@ const Home = () => {
         <div className="p-6">
             <h2 className="text-xl font-semibold">Home Component</h2>
             <p>Welcome to the Home page!</p>
+            <div>
+                <h1>      
+                    <SearchBar />
+                </h1>
+            </div>
+            
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     onClick={onTestReadingAll}>TEST ALL READINGS
             </button>
