c432bcc0b1538c0e70445dc7d6580d198897ecbf
Fonsitax <mynameisfonja@gmail.com>
2024-08-26 13:53:07 +0200

    setting numbers of bold letters

--- src/components/bionic-setting-menu.jsx
+++ src/components/bionic-setting-menu.jsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from "react";
 import BionicConverter from "./bionic-converter.jsx";
 
 
-const BionicReader = () => {
-    const [numberOfBoldLetters, setNumberOfBoldLetters] = useState(0,2,3); 
-    const [reading, setReading] = React.useState({});
+const BionicSettingMenu = ({ setNumberOfBoldLetters }) => {
+    // const [numberOfBoldLetters, setNumberOfBoldLetters] = useState(0, 2, 3);
+    // const [reading, setReading] = React.useState({});
 
 
     const handleButtonClick = (num) => {
         setNumberOfBoldLetters(num);
     };
 
-    const sampleText = `${reading.description} ${reading.text}`;
+    // const sampleText = `${reading.description} ${reading.text}`;
 
     return (
         <div>
@@ -20,13 +20,15 @@ const BionicReader = () => {
                 <button onClick={() => handleButtonClick(1)}>Bold First Letter</button>
                 <button onClick={() => handleButtonClick(3)}>Bold First 3 Letters</button>
             </div>
-            <div>
-                <BionicConverter text={sampleText} numberOfBoldLetters={numberOfBoldLetters} />
-            </div>
+            {/*<div>*/}
+            {/*    <BionicConverter*/}
+            {/*        text={text}*/}
+            {/*        numberOfBoldLetters={numberOfBoldLetters}/>*/}
+            {/*</div>*/}
         </div>
     );
 };
 
-export default BionicReader;
+export default BionicSettingMenu;
 
 
